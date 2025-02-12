eb695b9a30209e530113be87fddff52552b4fb92
channdararith <dararithzone@gmail.com>
2025-02-13 04:14:20 +0700

    test-only

--- React-Capstone/src/Components/Person.jsx
+++ React-Capstone/src/Components/Person.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Css/Person.css"
 function Person(){
-    return <p className="Person">Lorem ipsum dolor sit amet.</p>
+    return <p className="text-3xl font-bold underline">Lorem ipsum dolor sit amet.</p>
 }
 export default Person;
