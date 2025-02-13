b4bd51ec6de1a1b061e8b789b08f74a78c9c30d5
Chort Sereivathana <chortsereivathana@gmail.com>
2025-02-13 13:17:36 +0700

    First

---
+++ Pregnancy_Project/src/components/Header/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+            <nav>
+                <div>
+                    <span><img src="" alt="" /></span>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
---
+++ Pregnancy_Project/src/page/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Header/navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+}
+
+export default Home;
